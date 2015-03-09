f82cbefbd8da8f33e200e7d2eb324707177172b0
Matt Eland <Matt.Eland@Outlook.com>
2015-03-08 23:25:46 -0400

    Adding non-versioned files.

---
+++ ANIDataAggregationService/ANIDataAggregationService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ANIDataAggregationService
+{
+    public partial class ANIDataAggregationService : ServiceBase
+    {
+        public ANIDataAggregationService()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+        }
+
+        protected override void OnStop()
+        {
+        }
+    }
+}
