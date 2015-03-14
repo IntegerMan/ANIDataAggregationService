39e4fef6ef977ab354fac0fb67cd03176853c7ed
Matt Eland <Matt.Eland@Outlook.com>
2015-03-13 23:45:39 -0400

    Added preliminary support for traffic fetching. Gets traffic but doesn't interpret it yet.

---
+++ ANIDataAggregationService/Traffic/TrafficRecordingProcessor.cs
@@ -0,0 +1,43 @@
+﻿using System.IO;
+using System.Net;
+
+namespace ANIDataAggregationService.Traffic
+{
+    public class TrafficRecordingProcessor
+    {
+        private readonly string _bingMapsKey;
+
+        public TrafficRecordingProcessor(string bingMapsKey)
+        {
+            _bingMapsKey = bingMapsKey;
+        }
+
+        public void RecordTrafficIncidents(double westLongitude, double northLatitude, double eastLongitude, double southLatitude)
+        {
+
+            // Request Traffic from around the area.
+            var url = string.Format("http://dev.virtualearth.net/REST/v1/Traffic/Incidents/{0},{1},{2},{3}?key={4}",
+                eastLongitude,
+                northLatitude,
+                westLongitude,
+                southLatitude,
+                _bingMapsKey);
+
+            var request = WebRequest.Create(url);
+            var response = request.GetResponse();
+
+            // Read the response and get data
+            string responseData = null;
+            using (var stream = response.GetResponseStream())
+            {
+                if (stream != null)
+                {
+                    using (var reader = new StreamReader(stream))
+                    {
+                        responseData = reader.ReadToEnd();
+                    }
+                }
+            }
+        }
+    }
+}
--- ANIDataAggregationServiceConsoleTest/Program.cs
+++ ANIDataAggregationServiceConsoleTest/Program.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using ANIDataAggregationService;
+using ANIDataAggregationService.Traffic;
+using ANIDataAggregationServiceConsoleTest.Properties;
 
 namespace ANIDataAggregationServiceConsoleTest
 {
@@ -11,10 +13,21 @@ namespace ANIDataAggregationServiceConsoleTest
     {
         static void Main(string[] args)
         {
+            /*
             var zipCodes = AreaMonitor.GetWatchedZipCodes();
             var processor = new WeatherForecastRecordingProcessor(1, null, zipCodes);
             processor.RecordWeatherForecasts();
+            */
 
+            var processor = new TrafficRecordingProcessor(Settings.Default.BingMapsKey);
+
+            // Focus on the Columbus area
+            const double West = 40.198316;
+            const double North = -83.194528; 
+            const double East = 39.853391; 
+            const double South = -82.840906;
+
+            processor.RecordTrafficIncidents(West, North, East, South);
         }
     }
 }
